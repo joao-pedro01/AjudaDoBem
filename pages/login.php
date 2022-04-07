7425ee8f8c6ae0a6686f06480eb7b9da5c28fc45
joao-pedro01 <joao_pedro01@terra.com.br>
2022-04-07 13:26:27 -0300

    Add folder templates

---
+++ pages/login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../styles/login.css">
+</head>
+<body>
+<?php include_once "../templates/navbar.html";?>
+    <div class="wrapper">
+        <h2>Login</h2>
+        <p>Por favor, preencha os campos para fazer o login.</p>
+
+        <?php 
+        if(!empty($login_err)){
+            echo '<div class="alert alert-danger">' . $login_err . '</div>';
+        }        
+        ?>
+
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group">
+                <label>Nome do usuário</label>
+                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <label>Senha</label>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Entrar">
+            </div>
+            <p>Não tem uma conta? <a href="register.php">Inscreva-se agora</a>.</p>
+        </form>
+    </div>
+</body>
+</html>
