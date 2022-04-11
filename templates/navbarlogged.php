77180a327c6789173ffe1b93b9caa776ca8c5d27
joao-pedro01 <joao_pedro01@terra.com.br>
2022-04-11 17:02:27 -0300

    Add templates

---
+++ templates/navbarlogged.php
@@ -0,0 +1,25 @@
+<section id="header">
+    <a href="#"><img src="logo3.png" class="logo" alt="logo"></a>
+
+    <div>
+        <ul id="navbar">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="Doações.html">Doações</a></li>
+            <li><a href="Blog.html">Blog</a></li>
+            <li><a href="Sobre_nos.html">Sobre nós</a></li>
+            <li><a class="active" href="Contato.html">Contato</a></li>
+            <li>
+                <a href="Sacola.html">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-square" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                        <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z"/>
+                    </svg>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div id="mobile">
+        <a href="Sacola.html"><i class="far fa-shopping-bag"></i></a>
+        <i id="bar" class="fas fa-outdent"></i>
+    </div>
+</section>
