14f124a7e3b601b626e05af9937906ad94d354f0
joao-pedro01 <joao_pedro01@terra.com.br>
2022-07-03 11:55:56 -0300

    Update home.php

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -6,7 +6,7 @@
     <title>Ajuda do bem</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/src/views/styles/style.css">
+    <link rel="stylesheet" href="src/views/styles/style.css">
     <link rel="stylesheet" href="bootstrap/dist/css/bootstrap.min.css">
     <script src="bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
